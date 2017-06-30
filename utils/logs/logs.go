ff61364ba16c36e434ebcc5360aee91e4d7c0b63
joezou <yixian.zou@gmail.com>
2017-06-30 08:58:01 +0800

    resume log

--- utils/logs/logs.go
+++ utils/logs/logs.go
@@ -1,17 +1,18 @@
 package logs
 
 import (
+	"github.com/cihub/seelog"
 )
 
 func init(){
-	//logger, err := seelog.LoggerFromConfigAsFile("seelog.xml")
-	//
-	//if err != nil {
-	//	panic("init log fail,error!"+err.Error())
-	//}
-	//
-	//logger.SetAdditionalStackDepth(1)
-	//seelog.ReplaceLogger(logger)
-	//defer seelog.Flush()
+	logger, err := seelog.LoggerFromConfigAsFile("seelog.xml")
+
+	if err != nil {
+		panic("init log fail,error!"+err.Error())
+	}
+
+	logger.SetAdditionalStackDepth(1)
+	seelog.ReplaceLogger(logger)
+	defer seelog.Flush()
 }
 
