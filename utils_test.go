d82ea9e4d7481bb5902c11ef773ce7e2ad005016
joezou <yixian.zou@gmail.com>
2017-07-25 22:30:22 +0800

    add utils_test

--- utils_test.go
+++ utils_test.go
@@ -1,9 +1,50 @@
 package agollo
 
-import "testing"
+import (
+	"testing"
+	"strings"
+	"github.com/zouyx/agollo/test"
+)
 
 func TestGetInternal(t *testing.T) {
 	//fmt.Println("Usage of ./getmyip --get_ip=(external|internal)")
 	//flag.Parse()
-	t.Log(GetInternal())
+	ip:=GetInternal()
+
+	t.Log("Internal ip:",ip)
+	nums:=strings.Split(ip,".")
+
+	test.Equal(t,4,len(nums))
+}
+
+func TestIsEmpty(t *testing.T) {
+	flag:=IsEmpty("")
+	test.Equal(t,true,flag)
+
+	flag=IsEmpty("abc")
+	test.Equal(t,false,flag)
+}
+
+func TestIsNotEmpty(t *testing.T) {
+	flag:=IsNotEmpty("")
+	test.Equal(t,false,flag)
+
+	flag=IsNotEmpty("abc")
+	test.Equal(t,true,flag)
+}
+
+func TestIsNil(t *testing.T) {
+	flag:=IsNil(nil)
+	test.Equal(t,true,flag)
+
+	flag=IsNil("")
+	test.Equal(t,false,flag)
+}
+
+func TestIsNotNil(t *testing.T) {
+	flag:=IsNotNil(nil)
+	test.Equal(t,false,flag)
+
+	flag=IsNotNil("")
+	test.Equal(t,true,flag)
 }
