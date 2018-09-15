49795efa85e56857ea867d5c6cf599288f77e368
joezou <yixian.zou@gmail.com>
2018-09-15 23:20:21 +0800

    add json format config file

--- file.go
+++ file.go
@@ -1,1 +1,29 @@
 package agollo
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const FILE = "a.json"
+
+func writeConfigFile(config *ApolloConfig)error{
+	file, e := os.Create(FILE)
+	defer  file.Close()
+	if e!=nil{
+		return e
+	}
+
+	return json.NewEncoder(file).Encode(config)
+}
+
+func loadConfigFile() (*ApolloConfig,error){
+	file, e := os.Open(FILE)
+	defer  file.Close()
+	if e!=nil{
+		return nil,e
+	}
+	config:=&ApolloConfig{}
+	e=json.NewDecoder(file).Decode(config)
+	return config,e
+}
--- file_test.go
+++ file_test.go
@@ -1,1 +1,50 @@
 package agollo
+
+import (
+	"github.com/zouyx/agollo/test"
+	"testing"
+)
+
+func TestWriteConfigFile(t *testing.T) {
+	jsonStr := `{
+  "appId": "100004458",
+  "cluster": "default",
+  "namespaceName": "application",
+  "configurations": {
+    "key1":"value1",
+    "key2":"value2"
+  },
+  "releaseKey": "20170430092936-dee2d58e74515ff3"
+}`
+
+	config, err := createApolloConfigWithJson([]byte(jsonStr))
+
+	isNil(err)
+	e := writeConfigFile(config)
+	isNil(e)
+}
+
+func TestLoadConfigFile(t *testing.T) {
+	jsonStr := `{
+  "appId": "100004458",
+  "cluster": "default",
+  "namespaceName": "application",
+  "configurations": {
+    "key1":"value1",
+    "key2":"value2"
+  },
+  "releaseKey": "20170430092936-dee2d58e74515ff3"
+}`
+
+	config, err := createApolloConfigWithJson([]byte(jsonStr))
+
+	isNil(err)
+	newConfig,e := loadConfigFile()
+
+	t.Log(newConfig)
+	isNil(e)
+	test.Equal(t,config.AppId,newConfig.AppId)
+	test.Equal(t,config.ReleaseKey,newConfig.ReleaseKey)
+	test.Equal(t,config.Cluster,newConfig.Cluster)
+	test.Equal(t,config.NamespaceName,newConfig.NamespaceName)
+}
