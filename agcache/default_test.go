df8aff689b8ba452f9238ef384e9d2b5b65c9ec4
Joe Zou <yixian.zou@gmail.com>
2020-01-01 14:23:23 +0800

    add test case

--- agcache/default_test.go
+++ agcache/default_test.go
@@ -1,61 +1,47 @@
 package agcache
 
 import (
-	"errors"
-	"sync"
+	. "github.com/tevid/gohamcrest"
+	"testing"
 )
 
-//DefaultCache 默认缓存
-type DefaultCache struct {
-	defaultCache sync.Map
+var testDefaultCache CacheInterface
+
+func init() {
+	factory := &DefaultCacheFactory{}
+	testDefaultCache=factory.Create()
+
+	testDefaultCache.Set("a",[]byte("b"),100)
 }
 
-//Set 获取缓存
-func (d *DefaultCache) Set(key string, value []byte, expireSeconds int) (err error) {
-	d.defaultCache.Store(key, value)
-	return nil
+func TestDefaultCache_Set(t *testing.T) {
+	err := testDefaultCache.Set("k", []byte("c"), 100)
+	Assert(t,err,NilVal())
+	Assert(t,int64(2),Equal(testDefaultCache.EntryCount()))
 }
 
-//EntryCount 获取实体数量
-func (d *DefaultCache) EntryCount() (entryCount int64) {
-	count := int64(0)
-	d.defaultCache.Range(func(key, value interface{}) bool {
+func TestDefaultCache_Range(t *testing.T) {
+	var count int
+	testDefaultCache.Range(func(key, value interface{}) bool {
 		count++
 		return true
 	})
-	return count
+	Assert(t,2,Equal(count))
 }
 
-//Get 获取缓存
-func (d *DefaultCache) Get(key string) (value []byte, err error) {
-	v, ok := d.defaultCache.Load(key)
-	if !ok {
-		return nil, errors.New("load default cache fail")
-	}
-	return v.([]byte), nil
+func TestDefaultCache_Del(t *testing.T) {
+	b := testDefaultCache.Del("k")
+	Assert(t,true,Equal(b))
+	Assert(t,int64(1),Equal(testDefaultCache.EntryCount()))
 }
 
-//Range 遍历缓存
-func (d *DefaultCache) Range(f func(key, value interface{}) bool) {
-	d.defaultCache.Range(f)
+func TestDefaultCache_Get(t *testing.T) {
+	value, err := testDefaultCache.Get("a")
+	Assert(t,err,NilVal())
+	Assert(t,string(value),Equal("b"))
 }
 
-//Del 删除缓存
-func (d *DefaultCache) Del(key string) (affected bool) {
-	d.defaultCache.Delete(key)
-	return true
-}
-
-//Clear 清除所有缓存
-func (d *DefaultCache) Clear() {
-	d.defaultCache = sync.Map{}
-}
-
-//DefaultCacheFactory 构造默认缓存组件工厂类
-type DefaultCacheFactory struct {
-}
-
-//Create 创建默认缓存组件
-func (d *DefaultCacheFactory) Create() CacheInterface {
-	return &DefaultCache{}
+func TestDefaultCache_Clear(t *testing.T) {
+	testDefaultCache.Clear()
+	Assert(t,int64(0),Equal(testDefaultCache.EntryCount()))
 }
