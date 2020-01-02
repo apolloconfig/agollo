2c459ea4e445028863300f270460ef1a6ee08500
Joe Zou <yixian.zou@gmail.com>
2020-01-03 00:05:19 +0800

    add test case

--- storage/change_event_test.go
+++ storage/change_event_test.go
@@ -1,1 +1,60 @@
 package storage
+
+import (
+	. "github.com/tevid/gohamcrest"
+	"sync"
+	"testing"
+)
+
+var listener *CustomChangeListener
+
+func init() {
+	listener = &CustomChangeListener{}
+}
+
+type CustomChangeListener struct {
+	w sync.WaitGroup
+}
+
+func (t *CustomChangeListener) OnChange(event *ChangeEvent) {
+	t.w.Done()
+}
+
+func TestAddChangeListener(t *testing.T) {
+
+	AddChangeListener(listener)
+
+	Assert(t, changeListeners.Len(), Equal(1))
+}
+
+func TestGetChangeListeners(t *testing.T) {
+	Assert(t, GetChangeListeners().Len(), Equal(1))
+}
+
+func TestRemoveChangeListener(t *testing.T) {
+	RemoveChangeListener(listener)
+	Assert(t, changeListeners.Len(), Equal(0))
+}
+
+func TestPushChangeEvent(t *testing.T) {
+
+	addConfig := createAddConfigChange("new")
+	deleteConfig := createDeletedConfigChange("old")
+	modifyConfig := createModifyConfigChange("old", "new")
+	changes := make(map[string]*ConfigChange)
+	changes["add"] = addConfig
+	event := &ChangeEvent{
+		"a",
+		changes,
+	}
+	changes["delete"] = deleteConfig
+	changes["modify"] = modifyConfig
+	listener = &CustomChangeListener{}
+	listener.w.Add(1)
+
+	AddChangeListener(listener)
+
+	pushChangeEvent(event)
+
+	listener.w.Wait()
+}
