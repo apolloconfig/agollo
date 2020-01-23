514768a6f8f03996a2bae0ee0d33ef8b256ba425
Joe Zou <yixian.zou@gmail.com>
2020-01-24 00:01:30 +0800

    add test case

---
+++ loadbalance/load_balance_test.go
@@ -0,0 +1,25 @@
+package loadbalance
+
+import (
+	"sync"
+
+	"github.com/zouyx/agollo/v3/env/config"
+)
+
+var defaultLoadBalance LoadBalance
+
+//LoadBalance 负载均衡器
+type LoadBalance interface {
+	//Load 负载均衡，获取对应服务信息
+	Load(servers *sync.Map) *config.ServerInfo
+}
+
+//SetLoadBalance 设置负载均衡器
+func SetLoadBalance(loadBalance LoadBalance) {
+	defaultLoadBalance = loadBalance
+}
+
+//GetLoadBalance 获取负载均衡器
+func GetLoadBalance() LoadBalance {
+	return defaultLoadBalance
+}
